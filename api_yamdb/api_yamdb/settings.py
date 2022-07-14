969b389b197d8a3534c0ae46dfb36f8c3839fee9
Петр Пельков <pelkovpa@mail.ru>
2022-07-14 23:13:52 +0500

    change path to env

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 from dotenv import load_dotenv
 
-load_dotenv(dotenv_path="infra/.env")
+load_dotenv(dotenv_path=".env")
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
